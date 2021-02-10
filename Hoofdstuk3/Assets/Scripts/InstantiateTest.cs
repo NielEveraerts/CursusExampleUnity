51e2913d3e587fd475b03c3161aa5a9252b3e3cf
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-10 14:17:26 +0100

    Invoke, InvokeRepeating & CancelInvoke

--- Hoofdstuk3/Assets/Scripts/InstantiateTest.cs
+++ Hoofdstuk3/Assets/Scripts/InstantiateTest.cs
@@ -11,14 +11,28 @@ public class InstantiateTest : MonoBehaviour
     void Start()
     {
         //Instantiate(ball, transform.position, Quaternion.identity); //position is positie van het huidig gameonbject, kan dus ook iets anders zijn, Quaternion.identity betekent gewoon geen rotatie (0,0,0)
+    
+        Invoke("RandomBall", 5f);   //hiermee kan je de RandomBall functie 5 seconden na het straten activeren
+
+        InvokeRepeating("RandomBall", 2f, 1f);  //hiermee kan je de funcite na 2 seconden na het starten, elke seconde de functie doen
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if( Input.GetMouseButtonDown(0)){
+        //     int randomNumber = Random.Range(0,balls.Length);    //range(0,4) -> random getallen 0,1,2,3
+        //     Instantiate(balls[randomNumber], transform.position, Quaternion.identity);  //spawn dus random 1 van de 3 gekleurde ballen bij elke linkermuisklik
+        // }
+
         if( Input.GetMouseButtonDown(0)){
-            int randomNumber = Random.Range(0,balls.Length);    //range(0,4) -> random getallen 0,1,2,3
-            Instantiate(balls[randomNumber], transform.position, Quaternion.identity);  //spawn dus random 1 van de 3 gekleurde ballen bij elke linkermuisklik
+            print("Cancelled Generating");
+            CancelInvoke("RandomBall");     //stop de Invoke methodes die RandomBall activeren
         }
+    }
+
+    void RandomBall(){
+        int randomNumber = Random.Range(0,balls.Length);
+        Instantiate(balls[randomNumber], transform.position, Quaternion.identity);
     }
 }

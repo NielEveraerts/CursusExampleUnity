08f8c5ddd0ca8813b6ddce2303114505e97a9ada
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-10 15:43:40 +0100

    3.8 mouseInputs

--- Hoofdstuk3/Assets/Scripts/InputTest.cs
+++ Hoofdstuk3/Assets/Scripts/InputTest.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class InputTest : MonoBehaviour
 {
     public float speed = 10f;
+
+    public GameObject ball;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,9 +37,37 @@ public class InputTest : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal")*speed*Time.deltaTime; //time.deltaTime zorgt ervoor dat de waarde niet frame dependant is (anders gaat het anders aanvaoelen op verschillende pc's), het gaat de waarde naar speed/seconde omvormen
         float yInput = Input.GetAxis("Vertical")*speed*Time.deltaTime;
 
-        print(xInput);
+        //print(xInput);
 
         transform.Translate(xInput, yInput, 0);   
         Input.GetAxisRaw("Vertical");               //getAxis gaat geleidelijk van 0 naar 1, bij getAxisRaw gaat het direct van 0 naar 1 bij een input.
+
+        // if(Input.GetButtonDown("Fire1")){
+        //     print("Left Click");
+        // }
+
+        if(Input.GetMouseButtonDown(0)){           //0 = linker muisknop, 1 = rechtermuisknop, 2 = muiswiel indrukken
+            print("Left Click");
+            print("X: "+Input.mousePosition.x + ", Y: " + Input.mousePosition.y);   //dit is in screencoordinaten
+
+            Vector3 pos = Input.mousePosition;          //screencoordinaten, BELANGRIJK we moeten deze aanpassen naar worldcoordinaten
+            pos.z = 10f;                                //verplaats op z positie zodat heij niet in de camera gaat spawnen
+            pos = Camera.main.ScreenToWorldPoint(pos);  //hier converteren we de screencoordinaten naar WorldCoordinaten
+
+            Instantiate(ball , pos, Quaternion.identity);
+
+        }
+
+        if(Input.GetMouseButtonDown(1)){           //0 = linker muisknop, 1 = rechtermuisknop, 2 = muiswiel indrukken
+            print("Right Click");
+        }
+
+        if(Input.GetMouseButtonDown(2)){           //0 = linker muisknop, 1 = rechtermuisknop, 2 = muiswiel indrukken
+            print("Middle Click");
+        }
+    }
+
+    void OnMouseDown(){                     //funcite die detecteert wanneer op het object word geklikt, het script moet wel in het object zitten dat je wilt verwijderen als je erop klikt, ook moet het object een collider hebben
+        Destroy(gameObject);
     }
 }

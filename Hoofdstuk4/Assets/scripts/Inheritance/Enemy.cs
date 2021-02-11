5db6dff6d8b7440824964cb37b4e6cae99fa9d84
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-11 14:17:11 +0100

    4.6 inheritance deel1

---
+++ Hoofdstuk4/Assets/scripts/Inheritance/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    public void Attack(){
+        Debug.Log("Enemy Attack");
+    }
+
+    private void Iets(){            //dit kan niet gebruikt worden door classes die overdragen van Enemy
+
+    }
+}

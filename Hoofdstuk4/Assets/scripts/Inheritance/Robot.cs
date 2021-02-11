0922a28d75a7bbf9409f703c7721ffc3bb877bf7
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-11 14:55:10 +0100

    4.8 polymorfisme

--- Hoofdstuk4/Assets/scripts/Inheritance/Robot.cs
+++ Hoofdstuk4/Assets/scripts/Inheritance/Robot.cs
@@ -2,7 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Robot : Enemy
+public class Robot : MonoBehaviour
 {
+    private void Start(){
+        Robot robot = new Robot();
+        robot.Attack();
+
+        Robot robot2 = new BlueRobot();  //aangezien een BlueRobot een child is van Robot kunnen we zeggen dat Robot = new BlueRobot
+        robot2.Attack();
+
+        Robot robot3 = new RedRobot();
+        robot3.Attack();
+
+        Robot robotTest = new BlueRobot();  //aangezien robot geen Attack2 heeft en robotTest een Robot is ga je Attack2 van de blueRobot niet kunnen gebruiken
+        //robotTest.Attack2();
+    }
+    public virtual void Attack(){   //maak de standaard functie virtual zodat de childclasses de standaard attack kunnen overschrijven
+        print("Robot Attack");
+    }
+}
+
+public class BlueRobot : Robot {
+    public override void Attack(){  //overschrijf de standaard Attack functie
+        print("BlueRobot Attack");
+    }
+    public void Attack2(){
+        print("BlueRobot Attack2");
+    }
 
 }
+
+public class RedRobot : Robot {
+    public override void Attack(){
+        print("RedRobot Attack");
+    }
+}
+
+
+

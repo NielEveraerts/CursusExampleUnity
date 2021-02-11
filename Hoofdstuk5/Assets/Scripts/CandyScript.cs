6694ef19f050d70f8c9931db5bace657da34ea6c
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-11 22:10:50 +0100

    5.5 checken of speler de candy aanraakt

---
+++ Hoofdstuk5/Assets/Scripts/CandyScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){             //belangrijk dat je de functie juist staat
+        if(collider.gameObject.tag == "Player"){
+            //score++ ofzoiets
+            Destroy(gameObject);
+        }
+    }
+}

647db5bdb9f5f16efd91c4ea39a43b14e01a784f
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-12 13:26:39 +0100

    5.9 candies constant spawnen

--- Hoofdstuk5/Assets/Scripts/CandySpawner.cs
+++ Hoofdstuk5/Assets/Scripts/CandySpawner.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class CandySpawner : MonoBehaviour
 {
     [SerializeField]
-    float maxX;   
+    float maxX, spawnInterval;   
 
     public GameObject[] candies;  
+
+    public static CandySpawner instance;
+
+    private void Awake(){                   //static instance van deze klasse maken, zodat we overal aan de functies van deze klasse kunnen, soort van Manager klasse
+        if(instance == null){
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnCandy();
+        //SpawnCandy();
+        StartSpawning();
     }
 
     // Update is called once per frame
@@ -25,7 +35,31 @@ public class CandySpawner : MonoBehaviour
 
         float rndX = Random.Range(-maxX, maxX);
         Vector3 rndPos = new Vector3(rndX, transform.position.y, transform.position.z); //neem de random xPos en neem de y en z van het gameObject
-        
+
         Instantiate(candies[rnd], rndPos, transform.rotation);  //wat instantieren, waar, en in welke rotatie
     }
+
+    //mbv CoRoutines
+    IEnumerator SpawnCandies(){  
+        yield return new WaitForSeconds(2f);
+
+        while(true){
+            SpawnCandy();
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+
+    public void StartSpawning(){
+        StartCoroutine("SpawnCandies");
+    }
+
+    public void StopSpawning(){
+        StopCoroutine("SpawnCandies");
+    }
+
+    //maar zou ook gaan met InvokeRepeating
+    //InvokeRepeating("SpawnCandy", 2f, spawnInterval);
+    //CancelInvoke("SpawnCandy");
 }

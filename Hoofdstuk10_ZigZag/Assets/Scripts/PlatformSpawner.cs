190bc47bf42c18808f8766c941b39d0660693175
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-14 19:51:12 +0100

    10.8 platformen random spawnen wanneer het niet gameOver is

--- Hoofdstuk10_ZigZag/Assets/Scripts/PlatformSpawner.cs
+++ Hoofdstuk10_ZigZag/Assets/Scripts/PlatformSpawner.cs
@@ -7,22 +7,32 @@ public class PlatformSpawner : MonoBehaviour
     public GameObject platform;
     Vector3 lastPos;
     float size;
+
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
         size = platform.transform.localScale.x;
         lastPos = platform.transform.position;
 
-        for (int i = 0; i < 5; i++)
-        {
-            SpawnX();
-        }
+        InvokeRepeating("SpawnPlatforms", 2f, 0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameOver){
+            CancelInvoke("SpawnPlatforms");
+        }
+    }
+
+    void SpawnPlatforms(){
+        int rnd = Random.Range(0,2);
+        if(rnd == 0){
+            SpawnX();
+        }else{
+            SpawnZ();
+        }
     }
 
     void SpawnX(){

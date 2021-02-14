8a7bf973f220a936f7ee3ade6e1431e9593439d8
NielEveraerts <r0758943@student.thomasmore.be>
2021-02-14 16:24:11 +0100

    10.3 bal pas laten bewegen na eerste klik

--- Hoofdstuk10_ZigZag/Assets/Scripts/BallController.cs
+++ Hoofdstuk10_ZigZag/Assets/Scripts/BallController.cs
@@ -7,6 +7,8 @@ public class BallController : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    bool started = false;
+
     Rigidbody rb;
 
     void Awake(){       //is opgeroepen voor de awake functie
@@ -15,14 +17,22 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        rb.velocity = new Vector3(speed,0,0);   //start met in de x as een velocity van speed te geven.
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){       //bij elke linkermuisklik switchDirection()
-            SwitchDirection();
+        if(!started){
+            if(Input.GetMouseButtonDown(0)){       //bij elke linkermuisklik switchDirection()
+                rb.velocity = new Vector3(speed,0,0);   //start met in de x as een velocity van speed te geven.
+                started = true;
+            }
+        }
+        else{
+            if(Input.GetMouseButtonDown(0)){       //bij elke linkermuisklik switchDirection()
+                SwitchDirection();
+            }
         }
     }
 
